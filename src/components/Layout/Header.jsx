716944a9d830281714177a910c99045c2c2cafdb
Nourbol <nur6056@gmail.com>
2022-05-27 12:48:33 +0600

    Test commit

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="container">
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                 <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                    ZHANDOS!!!
+                    ZHANDOS
                 </a>
                 <nav>
                     <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
